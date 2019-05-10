94d02fb9e7deae3921f229a397feb73f739c0ee9
kawinCheng <707471634@qq.com>
2019-05-10 16:39:35 +0800

    1

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -72,11 +72,12 @@ class _LoginPageState extends State<LoginPage> {
         'phoneNumber': _phoneNum,
       }
     );
-
     if(result["state"]==0){
         _timer = new Timer(new Duration(seconds: 3), () {
           _showDialog("验证码为："+result["message"]+",有效时间为5分钟。");
         });
+    }else{
+       _showDialog(result["message"]);
     }
   }
 
@@ -106,7 +107,7 @@ class _LoginPageState extends State<LoginPage> {
       if(result['state']==0){
          Navigator.pushNamed(context, "indexPage");
       }else{
-
+         _showDialog(result["message"]);
       }
   }
 
