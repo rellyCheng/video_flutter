673f2b833086b3d5b7f0561ee7ccce95417377a2
rellyCheng <707471634@qq.com>
2019-05-21 22:49:01 +0800

    调试iOS真机

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:agora_flutter_quickstart/main.dart';
+import 'package:video_call_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
