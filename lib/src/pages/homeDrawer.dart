cd7ee9e7fc1ac18d6ed574e618ee54c8cfee81e0
kawinCheng <707471634@qq.com>
2019-06-09 17:13:20 +0800

    优化退出

--- lib/src/pages/homeDrawer.dart
+++ lib/src/pages/homeDrawer.dart
@@ -5,34 +5,34 @@ class HomeBuilder {
   static Widget homeDrawer() {
     return new ListView(padding: const EdgeInsets.only(), children: <Widget>[
       _drawerHeader(),
-      new ClipRect(
-        child: new ListTile(
-          leading: new CircleAvatar(child: new Text("A")),
-          title: new Text('Drawer item A'),
-          subtitle: new Text("Drawer item A subtitle"),
-          onTap: () => {},
-        ),
-      ),
+      // new ClipRect(
+      //   child: new ListTile(
+      //     leading: new CircleAvatar(child: new Text("A")),
+      //     title: new Text('Drawer item A'),
+      //     subtitle: new Text("Drawer item A subtitle"),
+      //     onTap: () => {},
+      //   ),
+      // ),
+      // new ListTile(
+      //   leading: new CircleAvatar(child: new Text("B")),
+      //   title: new Text('Drawer item B'),
+      //   subtitle: new Text("Drawer item B subtitle"),
+      //   onTap: () => {},
+      // ),
+      // new AboutListTile(
+      //   icon: new CircleAvatar(child: new Text("Ab")),
+      //   child: new Text("About"),
+      //   applicationName: "总而言之",
+      //   applicationVersion: "v1.0",
+      //   applicationIcon: new Image.asset(
+      //     "assets/images/match.gif",
+      //     width: 64.0,
+      //     height: 64.0,
+      //   ),
+      //   applicationLegalese: "app法律条文法律条文法律条文法律条文法律条文",
+      // ),
       new ListTile(
-        leading: new CircleAvatar(child: new Text("B")),
-        title: new Text('Drawer item B'),
-        subtitle: new Text("Drawer item B subtitle"),
-        onTap: () => {},
-      ),
-      new AboutListTile(
-        icon: new CircleAvatar(child: new Text("Ab")),
-        child: new Text("About"),
-        applicationName: "总而言之",
-        applicationVersion: "v1.0",
-        applicationIcon: new Image.asset(
-          "assets/images/match.gif",
-          width: 64.0,
-          height: 64.0,
-        ),
-        applicationLegalese: "app法律条文法律条文法律条文法律条文法律条文",
-      ),
-      new ListTile(
-        leading: new CircleAvatar(child: Icon(Icons.album)),
+        leading: new Icon(Icons.power_settings_new),
         title: new Text('退出'),
         onTap: () async{
           var prefs = await SharedPreferences.getInstance();
@@ -53,7 +53,7 @@ class HomeBuilder {
         "707471634@qq.com",
       ),
       currentAccountPicture: new CircleAvatar(
-        backgroundImage: new AssetImage("assets/images/match.gif",),
+        backgroundImage: new AssetImage("assets/images/hardon.jpg"),
       ),
       onDetailsPressed: () {
       },
