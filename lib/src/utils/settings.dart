2cd4e9fe939aeda7c6967a0cda27937b1a8890b7
kawinCheng <707471634@qq.com>
2019-06-13 16:27:10 +0800

    1

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,7 +1,7 @@
 // Agora AppId
 const APP_ID = "ae09cab9d1c34a3fab3b5fc4429f4e2c";
 const SOCKET_IP = "http://118.24.218.25:9091";
-const SERVER_IP = "http://118.24.218.25:8080";
+const SERVER_IP = "http://118.24.218.25:8081";
 
 // const SOCKET_IP = "http://192.168.1.160:9091";
 // const SERVER_IP = "http://192.168.1.160:8080";
